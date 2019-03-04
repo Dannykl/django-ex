876c0f2836d68b3127f818e4c96fb0a53daca87f
Daniel Niguse <Danielniguse8@gmail.com>
2019-03-04 16:03:59 +0000

    amend

---
+++ open-shift-blog/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/danielniguse/Documents/work_place/djangoProjects/django-ex/open-shift-blog/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
